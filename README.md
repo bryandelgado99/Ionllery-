675fed609e3f14bc3f180e614481325aa695232a
Bryan Paul Delgado <bryand9970@gmail.com>
2024-05-16 23:26:57 -0500

    Inicialización del proyecto IonGallery y correciones

---
+++ README.md
@@ -0,0 +1,27 @@
+# Ionllery - Ionic + Angular Photo Gallery
+
+## Descripción del proyecto
+
+## Participantes
+- Casillas Nataly
+- Delgado Bryan Paul
+- Miño Mateo
+
+## Antes de empezar
+Primero, se debe dirigir a la carpeta del proyecto con el comando <code>cd photo/gallery</code>.
+Posteriormente, se debe correr el comando <code>npm install o npm i</code> para poder instalar las dependencias o node_modules y poder utilizar todos los elementos proporcionados en la aplicación. 
+
+## Evidencias de Uso
+### Depuración de desarrollo
+
+## Dispositivo Real
+Android version: 12 ext 11
+SDK version: 31
+Device: TECNO Pova 4 
+
+## Conclusiones
+### Nataly Casillas
+
+### Bryan Delgado
+
+## Mateo Miño
---
+++ photo-gallery/src/app/tab1/tab1.page.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
+import { PhotoService, UserPhoto } from '../services/photo.service';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+
+  constructor(public photoService: PhotoService,
+    public actionSheetController: ActionSheetController) {}
+
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
+
+  async ngOnInit() {
+    await this.photoService.loadSaved();
+  }
+
+  public async showActionSheet(photo: UserPhoto, position: number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.photoService.deletePicture(photo, position);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          // Nothing to do, action sheet is automatically closed
+          }
+      }]
+    });
+    await actionSheet.present();
+  }
+}
